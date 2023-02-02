f6f8f61500ad04ffe2e4a0811cd3b1209b0e82fe
ilya <ilya123558ilya@gmail.com>
2023-02-02 15:54:30 +0300

    success

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-function twoSum(nums, target) {
-    for (let index = 0; index < nums.length; index++) {
-        for (let nextIndex = index + 1; nextIndex < nums.length; nextIndex++) {
-            if (nums[index] + nums[nextIndex] === target) {
-                return [index, nextIndex]
-            }
-        }
-    }
+const simbols = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
 }
 
-console.log(twoSum([2,7,11,15], 9))
+function romanToInt(s) {
+    let sum = 0
+    for (let index = 0; index < s.length; index++) {
+        if (s[index] == 'I' && (s[index + 1] === 'V' | s[index + 1] === 'X')) {
+            if (s[index + 1] === 'V') {
+                sum += 4
+                index += 1
+            }
+            else {
+                sum += 9
+                index += 1
+            }
+        }
+        else if (s[index] == 'X' && (s[index + 1] === 'L' | s[index + 1] === 'C')) {
+            if (s[index + 1] === 'L') {
+                sum += 40
+                index += 1
+            }
+            else {
+                sum += 90
+                index += 1
+            }
+        }
+        else if (s[index] == 'C' && (s[index + 1] === 'D' | s[index + 1] === 'M')) {
+            if (s[index + 1] === 'D') {
+                sum += 400
+                index += 1
+            }
+            else {
+                sum += 900
+                index += 1
+            }
+        }
+        else {
+            sum += simbols[s[index]]
+        }
+    }
+    return sum
+}
+
+console.log(romanToInt('MCMXCIV'))
