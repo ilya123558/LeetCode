c70791ef31843c1aab443fcad2b7e90afbdb2eb9
ilya <ilya123558ilya@gmail.com>
2023-02-02 16:01:21 +0300

    success

--- index.js
+++ index.js
@@ -1,51 +1,14 @@
-const simbols = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000
-}
+function getConcatenation(nums) {
 
-function romanToInt(s) {
-    let sum = 0
-    for (let index = 0; index < s.length; index++) {
-        if (s[index] == 'I' && (s[index + 1] === 'V' | s[index + 1] === 'X')) {
-            if (s[index + 1] === 'V') {
-                sum += 4
-                index += 1
-            }
-            else {
-                sum += 9
-                index += 1
-            }
-        }
-        else if (s[index] == 'X' && (s[index + 1] === 'L' | s[index + 1] === 'C')) {
-            if (s[index + 1] === 'L') {
-                sum += 40
-                index += 1
-            }
-            else {
-                sum += 90
-                index += 1
-            }
-        }
-        else if (s[index] == 'C' && (s[index + 1] === 'D' | s[index + 1] === 'M')) {
-            if (s[index + 1] === 'D') {
-                sum += 400
-                index += 1
-            }
-            else {
-                sum += 900
-                index += 1
-            }
-        }
-        else {
-            sum += simbols[s[index]]
+    const ans = []
+
+    for (let index = 0; index < 2; index++) {
+        for (let i = 0; i < nums.length; i++) {
+            ans.push(nums[i])
         }
     }
-    return sum
-}
 
-console.log(romanToInt('MCMXCIV'))
+    return ans
+};
+
+console.log(getConcatenation([1,3,2,1]))
