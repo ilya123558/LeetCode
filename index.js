39e2283e48bbdcc19178938060267502216e2f3d
ilya <ilya123558ilya@gmail.com>
2023-02-02 16:04:36 +0300

    success

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-function getConcatenation(nums) {
+var buildArray = function(nums) {
 
     const ans = []
 
-    for (let index = 0; index < 2; index++) {
-        for (let i = 0; i < nums.length; i++) {
-            ans.push(nums[i])
-        }
+    for (let i = 0; i < nums.length; i++) {
+        ans.push(nums[nums[i]])
     }
 
     return ans
 };
 
-console.log(getConcatenation([1,3,2,1]))
+console.log(buildArray([5,0,1,2,3,4]))
