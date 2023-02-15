d7fbccdce31dd94e768966d959d76e6b5d878ff8
ilya <ilya123558ilya@gmail.com>
2023-02-15 23:51:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function twoSum(nums, target) {
-    for (let index = 0; index < nums.length; index++) {
-        for (let nextIndex = index + 1; nextIndex < nums.length; nextIndex++) {
-            if (nums[index] + nums[nextIndex] === target) {
-                return [index, nextIndex]
-            }
-        }
+const arr = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
+
+function rotate(arrayWithNums) {
+
+    let result = Array(arrayWithNums.length).fill(1).map(() => Array(arrayWithNums.length).fill(null))
+
+    const arrLastIndex = arrayWithNums.length - 1
+
+    arrayWithNums.forEach((_, numsIndex) => {
+        arrayWithNums[numsIndex].forEach((num, numIndex) => {
+            const obj = { value: 0, lineIndex: 0, columnIndex: 0 }
+            obj.value = num
+            obj.lineIndex = arrLastIndex - arrLastIndex + numIndex
+            obj.columnIndex = arrLastIndex - numsIndex
+            result[obj.lineIndex][obj.columnIndex] = obj.value
+        })
+    })
+
+    for (let i = 0; i < arrayWithNums.length; i++) {
+        arrayWithNums[i] = result[i]
     }
+
 }
 
-console.log(twoSum([2,7,11,15], 9))
+rotate(arr)
+
+
+console.log(arr)
